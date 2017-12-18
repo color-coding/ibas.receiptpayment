3a4767688db942d803a541ea3fd06f35f32cfdab
牛加人等于朱 <niuren.zhu@icloud.com>
2017-12-18 17:09:40 +0800

    调整文件下载及浏览方式

--- ibas.receiptpayment.service/src/main/java/org/colorcoding/ibas/receiptpayment/service/rest/FileService.java
+++ ibas.receiptpayment.service/src/main/java/org/colorcoding/ibas/receiptpayment/service/rest/FileService.java
@@ -1,8 +1,8 @@
 package org.colorcoding.ibas.receiptpayment.service.rest;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
@@ -16,96 +16,93 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.colorcoding.ibas.bobas.common.Criteria;
 import org.colorcoding.ibas.bobas.common.ICondition;
 import org.colorcoding.ibas.bobas.common.IOperationResult;
 import org.colorcoding.ibas.bobas.common.OperationResult;
 import org.colorcoding.ibas.bobas.data.FileData;
-import org.colorcoding.ibas.bobas.i18n.I18N;
-import org.colorcoding.ibas.bobas.message.Logger;
-import org.colorcoding.ibas.bobas.repository.FileRepositoryReadonly;
+import org.colorcoding.ibas.bobas.repository.FileRepository;
 import org.colorcoding.ibas.bobas.repository.jersey.FileRepositoryService;
 import org.colorcoding.ibas.receiptpayment.MyConfiguration;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 @Path("file")
 public class FileService extends FileRepositoryService {
 
-    public final static String WORK_FOLDER = MyConfiguration.getConfigValue(
-            MyConfiguration.CONFIG_ITEM_RECEIPTPAYMENT_FILE_FOLDER,
-            MyConfiguration.getDataFolder() + File.separator + "receiptpayment_files");
+	public final static String WORK_FOLDER = MyConfiguration.getConfigValue(
+			MyConfiguration.CONFIG_ITEM_RECEIPTPAYMENT_FILE_FOLDER,
+			MyConfiguration.getDataFolder() + File.separator + "receiptpayment_files");
 
-    public FileService() {
-        // 设置工作目录
-        this.getRepository().setRepositoryFolder(FileService.WORK_FOLDER);
-    }
+	public FileService() {
+		// 设置工作目录
+		this.getRepository().setRepositoryFolder(FileService.WORK_FOLDER);
+	}
 
-    @POST
-    @Path("upload")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    public OperationResult<FileData> upload(@FormDataParam("file") InputStream fileStream,
-            @FormDataParam("file") FormDataContentDisposition fileDisposition, @QueryParam("token") String token) {
-        return super.save(fileStream, fileDisposition, token);
-    }
+	@POST
+	@Path("upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.APPLICATION_JSON)
+	public OperationResult<FileData> upload(@FormDataParam("file") InputStream fileStream,
+			@FormDataParam("file") FormDataContentDisposition fileDisposition, @QueryParam("token") String token) {
+		return super.save(fileStream, fileDisposition, token);
+	}
 
-    @POST
-    @Path("download")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public byte[] download(Criteria criteria, @QueryParam("token") String token,
-            @Context HttpServletResponse response) {
-        try {
-            // 获取文件
-            IOperationResult<FileData> operationResult = this.fetch(criteria, token);
-            if (operationResult.getError() != null) {
-                throw operationResult.getError();
-            }
-            if (operationResult.getResultCode() != 0) {
-                throw new Error(operationResult.getMessage());
-            }
-            FileData fileData = operationResult.getResultObjects().firstOrDefault();
-            if (fileData != null) {
-                // 数据存在，尝试转为字节数组
-                File file = new File(fileData.getLocation());
-                long fileSize = file.length();
-                if (fileSize > Integer.MAX_VALUE) {
-                    throw new Exception(I18N.prop("msg_bobas_invalid_data"));
-                }
-                FileInputStream inputStream = new FileInputStream(file);
-                byte[] buffer = new byte[(int) fileSize];
-                int offset = 0;
-                int numRead = 0;
-                while (offset < buffer.length
-                        && (numRead = inputStream.read(buffer, offset, buffer.length - offset)) >= 0) {
-                    offset += numRead;
-                }
-                inputStream.close();
-                response.setHeader("content-disposition",
-                        String.format("attachment;filename=%s", fileData.getFileName()));
-                return buffer;
-            } else {
-                // 无效的数据
-                response.setHeader("content-disposition", "attachment;filename=INVALID_DATA");
-                return new byte[] {};
-            }
-        } catch (Exception e) {
-            Logger.log(e);
-            throw new WebApplicationException(500);
-        }
-    }
+	@POST
+	@Path("download")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public byte[] download(Criteria criteria, @QueryParam("token") String token,
+			@Context HttpServletResponse response) {
+		try {
+			// 获取文件
+			IOperationResult<FileData> operationResult = this.fetch(criteria, token);
+			if (operationResult.getError() != null) {
+				throw operationResult.getError();
+			}
+			FileData fileData = operationResult.getResultObjects().firstOrDefault();
+			if (fileData != null) {
+				response.setHeader("content-disposition",
+						String.format("attachment;filename=%s", fileData.getFileName()));
+				return fileData.getFileBytes();
+			} else {
+				// 无效的导出数据
+				response.setHeader("content-disposition", "attachment;filename=INVALID_DATA");
+				return new byte[] {};
+			}
+		} catch (Exception e) {
+			throw new WebApplicationException(e);
+		}
+	}
 
-    @GET
-    @Path("{file}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public byte[] download(@PathParam("file") String file, @QueryParam("token") String token,
-            @Context HttpServletResponse response) {
-        Criteria criteria = new Criteria();
-        ICondition condition = criteria.getConditions().create();
-        condition.setAlias(FileRepositoryReadonly.CRITERIA_CONDITION_ALIAS_FILE_NAME);
-        condition.setValue(file);
-        return this.download(criteria, token, response);
-    }
+	@GET
+	@Path("{resource}")
+	public void resource(@PathParam("resource") String resource, @QueryParam("token") String token,
+			@Context HttpServletResponse response) {
+		try {
+			Criteria criteria = new Criteria();
+			ICondition condition = criteria.getConditions().create();
+			condition.setAlias(FileRepository.CRITERIA_CONDITION_ALIAS_FILE_NAME);
+			condition.setValue(resource);
+			// 获取文件
+			IOperationResult<FileData> operationResult = this.fetch(criteria, token);
+			if (operationResult.getError() != null) {
+				throw operationResult.getError();
+			}
+			FileData fileData = operationResult.getResultObjects().firstOrDefault();
+			if (fileData != null) {
+				// 写入响应输出流
+				OutputStream os = response.getOutputStream();
+				os.write(fileData.getFileBytes());
+				os.flush();
+			} else {
+				// 文件不存在
+				throw new WebApplicationException(404);
+			}
+		} catch (WebApplicationException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new WebApplicationException(e);
+		}
+	}
 }
