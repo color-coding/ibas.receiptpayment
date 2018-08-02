7335391913fb49d7dad46324cc5048b1a7f80f2b
牛加人等于朱 <niuren.zhu@icloud.com>
2018-08-02 16:45:29 +0800

    优化交互

--- ibas.receiptpayment.service/src/main/webapp/bsui/c/payment/PaymentChooseView.ts
+++ ibas.receiptpayment.service/src/main/webapp/bsui/c/payment/PaymentChooseView.ts
@@ -46,6 +46,17 @@ namespace receiptpayment {
                                 })
                             }),
                             new sap.ui.table.Column("", {
+                                label: ibas.i18n.prop("bo_payment_canceled"),
+                                template: new sap.m.Text("", {
+                                    wrapping: false
+                                }).bindProperty("text", {
+                                    path: "canceled",
+                                    formatter(data: any): any {
+                                        return ibas.enums.describe(ibas.emYesNo, data);
+                                    }
+                                })
+                            }),
+                            new sap.ui.table.Column("", {
                                 label: ibas.i18n.prop("bo_payment_documentstatus"),
                                 template: new sap.m.Text("", {
                                     wrapping: false
