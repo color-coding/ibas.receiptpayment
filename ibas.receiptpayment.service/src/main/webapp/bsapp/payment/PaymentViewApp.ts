1a8e211e4a4634e3a5c2eef76f4a68e450bff431
牛加人等于朱 <niuren.zhu@icloud.com>
2018-08-31 16:16:47 +0800

    优化交互

--- ibas.receiptpayment.service/src/main/webapp/bsapp/payment/PaymentViewApp.ts
+++ ibas.receiptpayment.service/src/main/webapp/bsapp/payment/PaymentViewApp.ts
@@ -34,6 +34,7 @@ namespace receiptpayment {
             /** 视图显示后 */
             protected viewShowed(): void {
                 // 视图加载完成
+                super.viewShowed();
             }
             /** 编辑数据，参数：目标数据 */
             protected editData(): void {
