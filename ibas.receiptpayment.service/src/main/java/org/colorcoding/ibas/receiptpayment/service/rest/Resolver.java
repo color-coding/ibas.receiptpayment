5324ba4d06d03bc9db6fa64cf9c443fbbe3f8ae8
Niuren.Zhu <niuren.zhu@icloud.com>
2026-08-13 18:00:08 +0800

    升级到 Java 21

--- ibas.receiptpayment.service/src/main/java/org/colorcoding/ibas/receiptpayment/service/rest/Resolver.java
+++ ibas.receiptpayment.service/src/main/java/org/colorcoding/ibas/receiptpayment/service/rest/Resolver.java
@@ -1,38 +1,12 @@
 package org.colorcoding.ibas.receiptpayment.service.rest;
 
-import javax.ws.rs.Produces;
-import javax.ws.rs.ext.ContextResolver;
-import javax.ws.rs.ext.Provider;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.ext.Provider;
 
-import org.colorcoding.ibas.bobas.bo.UserFieldProxy;
-import org.colorcoding.ibas.bobas.common.Criteria;
-import org.colorcoding.ibas.bobas.common.OperationResult;
-import org.colorcoding.ibas.businesspartner.bo.internalreconciliation.InternalReconciliation;
-import org.colorcoding.ibas.receiptpayment.bo.assetrecharge.AssetRecharge;
-import org.colorcoding.ibas.receiptpayment.bo.payment.Payment;
-import org.colorcoding.ibas.receiptpayment.bo.receipt.Receipt;
+import org.colorcoding.ibas.bobas.jersey.MoxyJsonContextResolver;
 
-/**
- * 序列化解释器
- */
 @Provider
-@Produces({ "application/json" })
-public class Resolver implements ContextResolver<JAXBContext> {
-
-	private static JAXBContext jaxbContext = null;
-
-	public JAXBContext getContext(Class<?> type) {
-		try {
-			if (jaxbContext == null) {
-				jaxbContext = JAXBContext.newInstance(Criteria.class, OperationResult.class, UserFieldProxy.class,
-						Payment.class, Receipt.class, AssetRecharge.class, InternalReconciliation.class);
-			}
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
-		return jaxbContext;
-	}
-
+@Produces(MediaType.APPLICATION_JSON)
+public class Resolver extends MoxyJsonContextResolver {
 }
